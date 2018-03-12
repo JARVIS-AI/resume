507a885b7805482a09bc54662222db5f58409850
Priansh Shah <shah@priansh.com>
2018-03-12 19:29:17 +0000

    doc rewrite

---
+++ console/console.js
@@ -0,0 +1,88 @@
+function type(/**/) { for (var i = 0; i < arguments.length; i++) $('#console_holder').append(arguments[i]); }
+
+function clear() { $('#console_holder').empty(); }
+
+var buffer = [];
+var iter = 0;
+
+function add_normal(x) {
+    buffer.unshift(x + "<br />");
+    console.log("added");
+}
+
+function add(x) {
+    buffer.unshift("> ");
+    y = x.split(" ");
+    for (var i = 0; i < y.length; i++)
+        buffer.unshift(y[i] + " ");
+    buffer.unshift("<br />");
+}
+
+function reorder() {
+    var temp = [];
+    for (var i = buffer.length; i > 0; i--)
+        temp.unshift(buffer[i - 1]);
+    for (var i = 0; i < buffer.length; i++)
+        buffer[i] = temp[i];
+    return buffer;
+}
+
+$(document).ready(function () {
+    console.log("Document is ready.");
+    add("Welcome to my personal resume.");
+    add("It's not like other resumes.");
+    add("I was inspired by bash and console/terminal environments.")
+    add("From here, you can browse links to my portfolio.");
+    add("I'm currently an MEng Computing student at Imperial College London.")
+    add_normal('')
+    add("Outside of school, I'm a startup founder!")
+    add_normal('I founded <a href="https://helloaiko.com" target="_blank">Aiko AI</a> and <a href="http://glemeapp.com">Gleme</a>.')
+    add_normal("");
+    add("I'm also very involved in the robotics community. Robotics has become one of my passions in the last few years.");
+    add_normal("<a target='_blank' href='robots/index.html'>Click here to view a list of my achievements in the field of robotics.</a>");
+    add_normal("");
+    add("And most of all, I'm a coder.")
+    add_normal("<a target='_blank' href='https://www.devpost.com/priansh'>Here are some selected projects.</a>")
+    add_normal("<a target='_blank' href='https://www.github.com/pshah123'>And here are all the projects that I've open sourced!</a>")
+    add_normal("")
+    add("Trying to get in contact with me professionally?")
+    add_normal("Check out my <a target='_blank' href='https://www.linkedin.com/in/priansh'>LinkedIn</a>!")
+    add_normal("")
+    add("I also love to write!")
+    add_normal("<a target='_blank' href='http://artandwriting.org/media/366221'>Here's my Scholastic Gold Medal piece from highschool.")
+    add_normal("Check out my <a target='_blank' href='https://www.medium.com/@priansh'>Medium</a> blog!")
+    add_normal("")
+    add("Loading kernel: | / - | \\ | 100%")
+    add_normal("")
+    add_normal("<script src=\"input_process.js\"></script>");
+    $("#console_holder").ready(function () {
+        type("thing");
+        type("i added this line as well");
+        type("how's it feel?");
+        clear();
+        type("<div class='coral'>Loading: Priansh Shah...");
+        type("<div>> Hello World!</div>");
+//        type("<div>> Keep typing with your keyboard to view the page.</div>"); //UNCOMMENT to use keypress!
+
+
+        buffer = reorder();
+    });
+    iter = buffer.length - 1;
+
+    function typeItOut() {
+        if (iter > -1) {
+            type(buffer[iter]); iter--;
+            setTimeout(typeItOut, 70);
+        }
+        else return;
+    }
+
+    setTimeout(typeItOut, 100); //comment this line out to use the keypress
+
+/* Uncomment this to use the keypress.
+    $(document).keypress(function () {
+        if (iter > -1)
+            type(buffer[iter]); iter--;
+    });
+*/
+});
---
+++ console/input_process.js
@@ -0,0 +1,81 @@
+var command = "";
+var commands = {
+    help: '<br />      |*|*|*|  Priansh OS 1.6.7 |*|*|*|<br /><br />List of available commands:<br />help - displays help text' +
+        '<br />hello - say hello!' +
+        '<br />email - get my email!' +
+        '<br />ls - list the contents of the current directory' +
+        '<br />python - if you know python this is for you' +
+        '<br />node - if you know JS this is for you',
+    ls: './<br /><a href="http://priansh.com">../</a><br /><a href="#">index.html</a><br /><a href="code/">code/</a><br /><a href="robots/">robots/</a><br /><a href="newspaper/">school/</a><br />js/<br />css/',
+    hello: 'Good morning!',
+    email: 'My email is <a href="mailto:shah@priansh.com">shah at priansh dot com</a> (trying my best to avoid bots!)',
+    python: '<a href="https://angel.co/aiko-ai/jobs">You might like this... :)</a>',
+    node: '<a href="https://angel.co/aiko-ai/jobs">You might like this... :)</a>',
+    bash: '<a href="https://angel.co/aiko-ai/jobs">You must think you\'re clever...</a>',
+    sh: '<a href="https://angel.co/aiko-ai/jobs">You must think you\'re clever...</a>',
+    sudo: '<a href="https://angel.co/aiko-ai/jobs">You must think you\'re clever...</a>',
+    su: '<a href="https://angel.co/aiko-ai/jobs">You must think you\'re clever...</a>',
+    cd: '<a href="https://angel.co/aiko-ai/jobs">You must think you\'re clever...</a>'
+};
+
+function type( /**/ ) {
+    for (var i = 0; i < arguments.length; i++) $('#console_holder').append(arguments[i]);
+}
+
+function assign() {
+    $('.cmd').ready(function () {
+        $('.cmd').focus();
+        $('.cmd').keypress(function (event) {
+            console.log("entered");
+            var keycode = (event.keyCode ? event.keyCode : event.which);
+            if (keycode == '13') {
+                command = $('.cmd').val().split(' ')[0];
+                $('input').removeClass('cmd');
+                $('input').prop("disabled", true);
+                //now, the input is stopped so we can input the result of the command
+                if (!(command in commands)) {
+                    type("<div></div><br />");
+                    type("Sorry, I wasn't able to find the keyword, file or script \"" + command + "\". Try again!");
+                    type(commands.help);
+                    type("<div></div><br /><br />");
+                } else {
+                    type("<div></div><br />");
+                    type(commands[command]);
+                    type("<div></div><br /><br />");
+                }
+                type("<div>> <input class=\"cmd\" name=\"command\" type=\"text\" placeholder=\"Type a command here...\"></input></div>");
+                assign();
+            }
+        });
+    });
+}
+
+$(document).ready(function () {
+    type(commands.help);
+    type("<div>> <input class=\"cmd\" name=\"command\" type=\"text\" placeholder=\"Type a command here...\"></input></div>");
+    $('.cmd').ready(function () {
+        $('.cmd').focus();
+        $('.cmd').keypress(function (event) {
+            console.log("entered");
+            var keycode = (event.keyCode ? event.keyCode : event.which);
+            if (keycode == '13') {
+                command = $('.cmd').val().split(' ')[0];
+                $('input').removeClass('cmd');
+                $('input').prop("disabled", true);
+                //now, the input is stopped so we can input the result of the command
+                if (!(command in commands)) {
+                    type("<div></div><br />");
+                    type("Sorry, I wasn't able to find the keyword, file or script \"" + command + "\". Try again!");
+                    type(commands.help);
+                    type("<div></div><br /><br />");
+                } else {
+                    type("<div></div><br />");
+                    type(commands[command]);
+                    type("<div></div><br /><br />");
+                }
+                type("<div>> <input class=\"cmd\" name=\"command\" type=\"text\" placeholder=\"Type a command here...\"></input></div>");
+                assign();
+            }
+        });
+    });
+});
